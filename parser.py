3e8bbc8bed447bd4dcb136d93eea0a1119f98918
tzidkik <kerentzidki2@gmail.com>
2021-12-03 14:27:04 +0200

    bla

--- parser.py
+++ parser.py
@@ -31,9 +31,12 @@ def is_prose(work_details):
     return False
 
 
-def get_author_id_and_work_name(work_details, work_html):
-    # Should return int and string
-    return 5, 'bla'
+def get_author_id(work_details, work_html):
+    pass
+
+
+def get_work_name(work_details, work_html):
+    pass
 
 
 def get_binding_book():
@@ -62,7 +65,8 @@ def parse_work(work_id):
     work_details = work_html.body.find_all('div', attrs={'class': 'work-details'})
     if not is_prose(work_details):
         return ""
-    author_id, work_name = get_author_id_and_work_name(work_details, work_html)
+    author_id = get_author_id(work_details, work_html)
+    work_name = get_work_name(work_details, work_html)
     author_link = author_link_prefix + str(author_id)
     author_response = requests.get(author_link)
     if author_response.status_code != 200:
